a4fb9fbad447f8794dcd97519d621d3bf08316e4
Evgeniy Trofimov <evgeniy.trofimov@weblabs.pro>
2024-05-24 15:58:55 +0700

    added error handling

--- src/hooks/useAddMessageQuery.js
+++ src/hooks/useAddMessageQuery.js
@@ -1,14 +1,26 @@
 import { useCallback } from 'react';
 import { HTTP_SERVER } from '../consts.js';
 
-export const useAddMessageQuery = (ref) => useCallback(() => {
-  fetch(`${HTTP_SERVER.PROTOCOL}://${HTTP_SERVER.HOST}:${HTTP_SERVER.PORT}/addMessage`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify({ message: ref.current.value }),
-  });
-  ref.current.focus();
-  ref.current.value = '';
+export const useAddMessageQuery = (ref) => useCallback(async () => {
+  try {
+    const response = await fetch(
+      `${HTTP_SERVER.PROTOCOL}://${HTTP_SERVER.HOST}:${HTTP_SERVER.PORT}/addMessage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({ message: ref.current.value }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    ref.current.focus();
+    ref.current.value = '';
+  } catch (error) {
+    console.error(error);
+  }
 }, [ref]);
--- src/hooks/useMessagesQueries.js
+++ src/hooks/useMessagesQueries.js
@@ -6,15 +6,30 @@ export const useMessagesQueries = (dispatch) => {
   const queryClient = useQueryClient();
   
   useEffect(() => {
-    const ws = new WebSocket(`${WS_SERVER.PROTOCOL}://${WS_SERVER.HOST}:${WS_SERVER.PORT}`);
-    ws.onmessage = ({ data }) => queryClient.setQueriesData('action', JSON.parse(data));
+    try {
+      const ws = new WebSocket(`${WS_SERVER.PROTOCOL}://${WS_SERVER.HOST}:${WS_SERVER.PORT}`);
 
-    return () => ws.close();
+      ws.onmessage = ({ data }) => queryClient.setQueriesData('action', JSON.parse(data));
+  
+      return () => ws.readyState === 1 && ws.close();
+    } catch (error) {
+      console.error(error);
+    }
   }, [queryClient]);
 
-  const { data: action } = useQuery('action', () =>
-    fetch(`${HTTP_SERVER.PROTOCOL}://${HTTP_SERVER.HOST}:${HTTP_SERVER.PORT}/getAllMessages`).then((res) => res.json())
-  );
+  const { data: action } = useQuery('action', async () => {
+    try {
+      const response = await fetch(`${HTTP_SERVER.PROTOCOL}://${HTTP_SERVER.HOST}:${HTTP_SERVER.PORT}/getAllMessages`);
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   useEffect(() => dispatch(action), [action, dispatch]);
 };
